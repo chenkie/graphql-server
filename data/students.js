e2684c7b301ea53a782df905e248bf90c292c683
Ryan Chenkie <ryanchenkie@gmail.com>
2017-10-09 11:52:16 -0400

    adjust data

--- data/students.js
+++ data/students.js
@@ -4,13 +4,39 @@ module.exports = [
     firstName: 'John',
     lastName: 'Doe',
     active: true,
-    courses: []
+    courses: [
+      {
+        id: 1,
+        name: 'Machine Learning',
+        description: 'Learn machines',
+        level: '3'
+      },
+      {
+        id: 2,
+        name: 'Intro to HTML',
+        description: 'Start somewhere',
+        level: '1'
+      }
+    ]
   },
   {
     id: 2,
     firstName: 'Jane',
     lastName: 'Smith',
     active: true,
-    courses: []
+    courses: [
+      {
+        id: 1,
+        name: 'Machine Learning',
+        description: 'Learn machines',
+        level: '3'
+      },
+      {
+        id: 3,
+        name: 'Functional JavaScript',
+        description: 'Because FUN!',
+        level: '5'
+      }
+    ]
   }
 ];
