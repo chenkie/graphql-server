cda943243c92071f0271f77f85aa61be7a537ec1
Ryan Chenkie <ryanchenkie@gmail.com>
2017-10-09 18:31:59 -0500

    add args-resolvers start

--- server.js
+++ server.js
@@ -27,17 +27,12 @@ const StudentType = new gql.GraphQLObjectType({
     firstName: { type: gql.GraphQLString },
     lastName: { type: gql.GraphQLString },
     active: { type: gql.GraphQLBoolean },
-    courses: {
-      type: new gql.GraphQLList(CourseType),
-      args: {
-        level: { type: new gql.GraphQLNonNull(gql.GraphQLString) }
-      },
-      resolve(parent, { level }) {
-        return parent.courses.filter(course => {
-          return course.level === level;
-        });
-      }
-    }
+
+    // TODO: make the courses field require an argument
+    // called 'level' which filters out the courses array
+    // based on that value. Use the parent (root) data in the
+    // resolve function to get access to the proper data
+    courses: { ... }
   }
 });
 
@@ -57,40 +52,22 @@ const schema = new gql.GraphQLSchema({
           return STUDENTS;
         }
       },
-      courseById: {
-        type: CourseType,
-        args: {
-          id: { type: new gql.GraphQLNonNull(gql.GraphQLID) }
-        },
-        resolve(_, { id }) {
-          return COURSES.find(course => {
-            return course.id === id;
-          });
-        }
-      },
-      studentById: {
-        type: StudentType,
-        args: {
-          id: { type: new gql.GraphQLNonNull(gql.GraphQLID) }
-        },
-        resolve(_, { id }) {
-          return STUDENTS.find(student => {
-            return student.id === id;
-          });
-        }
-      },
-      searchStudentsByName: {
-        type: new gql.GraphQLList(StudentType),
-        args: {
-          name: { type: new gql.GraphQLNonNull(gql.GraphQLString) }
-        },
-        resolve(_, { name }) {
-          const pattern = new RegExp(name);
-          return STUDENTS.filter(student => {
-            return pattern.test(student.lastName);
-          });
-        }
-      }
+      
+      // TODO: make this new 'courseById' field search for a
+      // course based on the specific ID passed as an argument.
+      // Use the JavaScript find method to get the result
+      courseById: { ... },
+
+      // TODO: make this new 'studentById' field search for a
+      // student based on the specific ID passed as an argument.
+      // Use the JavaScript find method to get the result
+      studentById: { ... },
+
+      // TODO: make this new 'searchStudentByName' field search for
+      // students by last name based on the 'name' argument passed in.
+      // Use a RegExp and the JavaScript filter method to look for
+      // students that match (or partially match) the name
+      searchStudentsByName: { ... }
     }
   })
 });
