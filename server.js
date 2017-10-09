a56519f775359d68c367ff95d970891cb57f19a1
Ryan Chenkie <ryanchenkie@gmail.com>
2017-10-09 11:51:10 -0400

    add data modeling start

--- server.js
+++ server.js
@@ -10,46 +10,22 @@ const app = express();
 const COURSES = require('./data/courses');
 const STUDENTS = require('./data/students');
 
-const CourseType = new gql.GraphQLObjectType({
-  name: 'CourseType',
-  fields: {
-    id: { type: gql.GraphQLID },
-    name: { type: gql.GraphQLString },
-    description: { type: gql.GraphQLString },
-    level: { type: gql.GraphQLString }
-  }
-});
+// TODO: Create a GraphQLObjectType which holds fields
+// that describes the data in the data/courses.js file
+const CourseType = // ...
 
-const StudentType = new gql.GraphQLObjectType({
-  name: 'StudentType',
-  fields: {
-    id: { type: gql.GraphQLID },
-    firstName: { type: gql.GraphQLString },
-    lastName: { type: gql.GraphQLString },
-    active: { type: gql.GraphQLBoolean },
-    courses: { type: new gql.GraphQLList(CourseType) }
-  }
-});
+// TODO: Create a GraphQLObjectType which holds fields
+// that describes the data in the data/students.js file
 
-const schema = new gql.GraphQLSchema({
-  query: new gql.GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      allCourses: {
-        type: new gql.GraphQLList(CourseType),
-        resolve() {
-          return COURSES;
-        }
-      },
-      allStudents: {
-        type: new gql.GraphQLList(StudentType),
-        resolve() {
-          return STUDENTS;
-        }
-      }
-    }
-  })
-});
+// NOTE: think carefully about how to model the `courses` field
+const StudentType = // ...
+
+// TODO: Create a GraphQLSchema which holds
+// a root query type. The root query type should
+// have fields used to get a list of all the courses
+// and all the students. Use the `resolve` function
+// to return the data when it is queried
+const schema = // ...
 
 app.use(
   '/graphql',
