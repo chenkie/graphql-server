3899b0b2e3ce49d336ba0314f73b405a74ea06b4
Ryan Chenkie <ryanchenkie@gmail.com>
2017-10-09 11:50:04 -0400

    add data modeling finish

--- data/courses.js
+++ data/courses.js
@@ -3,18 +3,18 @@ module.exports = [
     id: 1,
     name: 'Machine Learning',
     description: 'Learn machines',
-    level: ''
+    level: '5'
   },
   {
     id: 2,
     name: 'Intro to HTML',
     description: 'Start somewhere',
-    level: ''
+    level: '1'
   },
   {
     id: 3,
     name: 'Functional JavaScript',
     description: 'Because FUN!',
-    level: ''
+    level: '3'
   }
 ];
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const gql = require('graphql');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 const port = process.env.PORT || 8080;
@@ -33,50 +31,6 @@ const StudentType = new gql.GraphQLObjectType({
   }
 });
 
-// Use the GraphQLInputObject types when moving everything over
-// to a single input variable
-
-// const CourseInputType = new gql.GraphQLInputObjectType({
-//   name: 'CourseInputType',
-//   fields: {
-//     name: { type: gql.GraphQLString },
-//     description: { type: gql.GraphQLString },
-//     level: { type: gql.GraphQLString }
-//   }
-// });
-
-// const StudentInputType = new gql.GraphQLInputObjectType({
-//   name: 'StudentInputType',
-//   fields: {
-//     firstName: { type: gql.GraphQLString },
-//     lastName: { type: gql.GraphQLString },
-//     active: { type: gql.GraphQLBoolean },
-//     courseIds: { type: gql.GraphQLString }
-//   }
-// });
-
-const CourseSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: false },
-  level: { type: String, required: false }
-});
-
-const StudentSchema = mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  active: { type: Boolean, required: true },
-  courses: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'course',
-      required: true
-    }
-  ]
-});
-
-const Course = mongoose.model('course', CourseSchema);
-const Student = mongoose.model('student', StudentSchema);
-
 const schema = new gql.GraphQLSchema({
   query: new gql.GraphQLObjectType({
     name: 'RootQueryType',
@@ -84,103 +38,13 @@ const schema = new gql.GraphQLSchema({
       allCourses: {
         type: new gql.GraphQLList(CourseType),
         resolve() {
-          return Promise.resolve(Course.find({}));
+          return COURSES;
         }
       },
       allStudents: {
         type: new gql.GraphQLList(StudentType),
         resolve() {
-          return Promise.resolve(Student.find({}));
-        }
-      }
-    }
-  }),
-  mutation: new gql.GraphQLObjectType({
-    name: 'RootMutationType',
-    fields: {
-      createCourse: {
-        type: CourseType,
-        args: {
-          name: { type: new gql.GraphQLNonNull(gql.GraphQLString) },
-          description: { type: gql.GraphQLString },
-          level: { type: gql.GraphQLString }
-        },
-        resolve(_, { name, description, level }) {
-          const input = { name, description, level };
-          const course = new Course(input);
-          return Promise.resolve(course.save());
-        }
-      },
-      updateCourse: {
-        type: CourseType,
-        args: {
-          id: { type: new gql.GraphQLNonNull(gql.GraphQLID) },
-          name: { type: new gql.GraphQLNonNull(gql.GraphQLString) },
-          description: { type: gql.GraphQLString },
-          level: { type: gql.GraphQLString }
-        },
-        resolve(_, { id, name, description, level }) {
-          const input = { name, description, level };
-          return Promise.resolve(
-            Course.findOneAndUpdate({ _id: id }, input, { new: true })
-          );
-        }
-      },
-      deleteCourse: {
-        type: CourseType,
-        args: {
-          id: { type: new gql.GraphQLNonNull(gql.GraphQLID) }
-        },
-        resolve(_, { id }) {
-          return Promise.resolve(Course.findOneAndRemove({ _id: id }));
-        }
-      },
-      createStudent: {
-        type: StudentType,
-        args: {
-          firstName: { type: new gql.GraphQLNonNull(gql.GraphQLString) },
-          lastName: { type: new gql.GraphQLNonNull(gql.GraphQLString) },
-          active: { type: new gql.GraphQLNonNull(gql.GraphQLBoolean) },
-          courses: {
-            type: new gql.GraphQLNonNull(new gql.GraphQLList(gql.GraphQLString))
-          }
-        },
-        resolve(_, { firstName, lastName, active, courses }) {
-          const input = { firstName, lastName, active, courses };
-          const student = new Student(input);
-          student.save();
-          return Promise.resolve(
-            Student.populate(student, { path: 'courses' })
-          );
-        }
-      },
-      updateStudent: {
-        type: StudentType,
-        args: {
-          id: { type: new gql.GraphQLNonNull(gql.GraphQLID) },
-          firstName: { type: new gql.GraphQLNonNull(gql.GraphQLString) },
-          lastName: { type: new gql.GraphQLNonNull(gql.GraphQLString) },
-          active: { type: new gql.GraphQLNonNull(gql.GraphQLBoolean) },
-          courses: { type: new gql.GraphQLList(gql.GraphQLString) }
-        },
-        resolve(_, { id, firstName, lastName, active, courses }) {
-          const input = { firstName, lastName, active, courses };
-          return Promise.resolve(
-            Student.findOneAndUpdate({ _id: id }, input, {
-              new: true
-            }).populate('courses')
-          );
-        }
-      },
-      deleteStudent: {
-        type: StudentType,
-        args: {
-          id: { type: new gql.GraphQLNonNull(gql.GraphQLID) }
-        },
-        resolve(_, { id }) {
-          return Promise.resolve(
-            Student.findOneAndRemove({ _id: id }).populate('courses')
-          );
+          return STUDENTS;
         }
       }
     }
@@ -196,24 +60,5 @@ app.use(
   })
 );
 
-function listen() {
-  app.listen(port);
-  console.log('Express app started on port ' + port);
-}
-
-async function connect() {
-  let options = {
-    useMongoClient: true,
-    keepAlive: true,
-    reconnectTries: 30,
-    socketTimeoutMS: 0
-  };
-  try {
-    await mongoose.connect(process.env.MLAB_URL, options);
-  } catch (err) {
-    console.log(err);
-  }
-  listen();
-}
-
-connect();
+app.listen(port);
+console.log(`Server listening on localhost:${port}`);
